fcc270186b4d39a3f0c11398ef363c4ee022445f
sdgstscsth <sdgstscsth@yahoo.com>
2012-08-19 23:21:15 -0500

    add a main method

---
+++ src/Sample.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: hung
+ * Date: 8/19/12
+ * Time: 11:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sample {
+
+    public static void main(String args[]){
+
+    }
+}
